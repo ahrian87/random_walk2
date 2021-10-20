69e2a460f0d6df72311182dd9ce2cbc41d7d7ff0
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-20 19:49:32 +0200

    Defining fill_walk method for generating all points for random walk

--- random_walk.py
+++ random_walk.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 class RandomWalk():
     """"Class for generating random walk"""
 
@@ -10,3 +11,27 @@ class RandomWalk():
         # Starting point is (0,0)
         self.x_values = [0]
         self.y_values = [0]
+
+    def fill_walk(self):
+        """Generating all points for random walk"""
+        # Making steps till expected number of points
+        while len(self.x_values) < self.num_points:
+
+            # Choosing the direction and distance to walk
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Establishing next X and Y
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
