7edc6598795ae51301107b57c2959a99dfd80dc9
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-20 20:35:08 +0200

    Starting and ending points of random walk are being showed in different colors

--- rw_visual.py
+++ rw_visual.py
@@ -13,6 +13,9 @@ while True:
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor="none", s=15)
+    # Showing starting and ending point with different colors
+    ax.scatter(0, 0, c="green", edgecolors="none", s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
     plt.show()
     keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
     if keep_running == "n":
