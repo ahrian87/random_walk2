82795a957e13e9cfaf60299392b4a58efe44b213
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-20 20:40:41 +0200

    Hid the axes

--- rw_visual.py
+++ rw_visual.py
@@ -16,6 +16,11 @@ while True:
     # Showing starting and ending point with different colors
     ax.scatter(0, 0, c="green", edgecolors="none", s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+
+    # Hiding the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
     keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
     if keep_running == "n":
