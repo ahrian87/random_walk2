db0cb6a232e055737407aac10a3c9eca142ca0f7
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-20 19:42:16 +0200

    Made a random attributes initializing method

--- random_walk.py
+++ random_walk.py
@@ -1,3 +1,12 @@
 from random import choice
 
 class RandomWalk():
+    """"Class for generating random walk"""
+
+    def __init__(self, num_points=5000):
+        """Random attributes initialization"""
+        self.num_points = num_points
+
+        # Starting point is (0,0)
+        self.x_values = [0]
+        self.y_values = [0]
