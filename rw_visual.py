7ecebb66a8b65588c0ffe61d4d10392fcd724252
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-20 20:03:22 +0200

    Changed the file, so that user can have another random walk generated if he wishes to, otherwise they can type "n" to break the while loop

--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Preparing random walk data
-rw = RandomWalk()
-rw.fill_walk()
+# Making a random walk while the program stays active
+while True:
+    # Preparing random walk data
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Displaying random walk points
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Displaying random walk points
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+    keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
+    if keep_running == "n":
+        break
