0947bef59ab926c77273df989a4b205e90489f71
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-20 20:43:24 +0200

    I try to experiment with the number of points to visualize

--- rw_visual.py
+++ rw_visual.py
@@ -5,14 +5,14 @@ from random_walk import RandomWalk
 # Making a random walk while the program stays active
 while True:
     # Preparing random walk data
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Displaying random walk points
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor="none", s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor="none", s=1)
     # Showing starting and ending point with different colors
     ax.scatter(0, 0, c="green", edgecolors="none", s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
